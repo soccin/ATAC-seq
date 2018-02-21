141567cfb561164f11261ef7ef877730ef41cc91
Nicholas D. Socci <soccin@mskcc.org>
2018-02-20 21:13:42 -0500

    First two stages in pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -30,19 +30,18 @@ if [ "$#" -lt "1" ]; then
 fi
 
 BAMS=$*
+echo SDIR=$SDIR
+echo BAMS=$BAMS
 
-echo $BAMS
 
-exit
+echo $BAMS \
+    | xargs -n 1 bsub -o LSF.POST/ -J ${TAG}_POST2_$$ -R "rusage[mem=24]" -M 25 $SDIR/postMapBamProcessingPE_ATACSeq.sh
 
-ls ../results/Proj_06125_*/*/align*/*.bam \
-    | xargs -n 1 bsub -o LSF.POST/ -J POST2_$$ -R "rusage[mem=24]" -M 25 ../postMapBamProcessing_ATACSeq.sh
-
-bSync POST2_$$
+bSync ${TAG}_POST2_$$
 ls *.bed.gz \
-    | xargs -n 1 bsub -o LSF.CALLP/ -J CALLP2_$$ -n 3 -R "rusage[mem=16]" -M 17 ../callPeaks_ATACSeq.sh
+    | xargs -n 1 bsub -o LSF.CALLP/ -J ${TAG}_CALLP2_$$ -n 3 -R "rusage[mem=16]" -M 17 $SDIR/callPeaksPE_ATACSeq.sh
 
-bSync CALLP2_$$
-ls callpeaks/*/*narrow* | fgrep s_N \
-    | xargs bsub -o LSF.IDR/ -J IDR -R "rusage[mem=8]" -M 9 ../runIDR.sh
+# bSync CALLP2_$$
+# ls callpeaks/*/*narrow* | fgrep s_N \
+#     | xargs bsub -o LSF.IDR/ -J IDR -R "rusage[mem=8]" -M 9 ../runIDR.sh
 
