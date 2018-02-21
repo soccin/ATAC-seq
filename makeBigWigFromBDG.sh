827f1fa940caae836c041470db3d221e332a1fe4
Nicholas D. Socci <soccin@mskcc.org>
2018-02-20 21:14:04 -0500

    Added code to convert BD to BigWig

---
+++ makeBigWigFromBDG.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+BDG=$1
+OUT=$(basename $BDG | sed 's/.bdg/.bw/')
+
+GENOME=mouse_mm10.genome
+
+TDIR=/scratch/socci
+mkdir -p $TDIR
+TMP=$(mktemp -p $TDIR)
+echo $TMP, $OUT
+bedtools intersect -a $BDG -b ${GENOME}.bed -wa | sort -k1,1V -k2,2n -S 16G >$TMP
+$SDIR/bedGraphToBigWig $TMP $GENOME $OUT
+
+
