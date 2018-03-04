a708f56d6abc8b76444ddabbc07415263d8d46fa
Nicholas D. Socci <soccin@mskcc.org>
2018-03-04 17:43:05 -0500

    Added back chromosome filter

--- postMapBamProcessing_ATACSeq.sh
+++ postMapBamProcessing_ATACSeq.sh
@@ -23,10 +23,12 @@ samtools view -q 10 -f 3 -F 1804 $IBAM -u >$TDIR/step1.bam
 picardV2 SortSam I=$TDIR/step1.bam O=$OBAM SO=coordinate MAX_RECORDS_IN_RAM=5000000
 
 #
-# Do Tn5 shift
+# Do Tn5 shift and remove non-standard chromosomes
+#
 
 samtools view -b $OBAM \
     | bedtools bamtobed -i - \
+    | egrep -v "chrUn|_random" \
     | awk -F'\t' \
         'BEGIN {OFS = FS} { if ($6 == "+") {$2 = $2 + 4} else if ($6 == "-") {$3 = $3 - 5} print $0}' \
     | gzip -nc >${OBAM/.bam/.shifted.bed.gz}
