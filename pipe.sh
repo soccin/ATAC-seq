2dc110461cea60ef317a73d2ff39bc681bb80d0c
Nicholas D. Socci <soccin@mskcc.org>
2022-04-01 20:37:03 -0400

    Added -q option for MAPQ and INS stats plot

--- pipe.sh
+++ pipe.sh
@@ -22,6 +22,32 @@ PIPENAME="ATAC-Seq"
 ##
 # Process command args
 
+MAPQ=10
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -q|--mapq)
+      MAPQ="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+echo "MAPQ = ${MAPQ}"
+
 TAG=q$PIPENAME
 
 COMMAND_LINE=$*
@@ -54,10 +80,14 @@ if [[ $GENOME =~ unknown ]]; then
 fi
 
 RUNTIME="-W 359"
+
 echo $BAMS \
-    | xargs -n 1 bsub $RUNTIME -o LSF.01.POST/ -J ${TAG}_POST2_$$ -R "rusage[mem=24]" $SDIR/postMapBamProcessing_ATACSeq.sh
+    | xargs -n 1 bsub $RUNTIME -o LSF.01.POST/ -J ${TAG}_POST2_$$ -R "rusage[mem=24]" \
+        $SDIR/postMapBamProcessing_ATACSeq.sh -q $MAPQ
 
 bSync ${TAG}_POST2_$$
+
+Rscript --no-save $SDIR/plotINSStats.R
 
 ls *.bed.gz \
     | xargs -n 1 bsub $RUNTIME -o LSF.02.BW/ -J ${TAG}_BW2_$$ -R "rusage[mem=24]" $SDIR/makeBigWigFromBEDZ.sh $GENOME
