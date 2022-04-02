5ae1fa2efc599441248f42ac791c412c18ea05ae
Nicholas D. Socci <soccin@mskcc.org>
2022-04-02 13:09:17 -0400

    Added density ins plot

--- plotINSStats.R
+++ plotINSStats.R
@@ -10,13 +10,22 @@ suppressPackageStartupMessages(require(tidyverse))
 
 
 insFiles=dir(pattern="___INS.txt")
-dd=map(insFiles,read_insdat) %>% bind_rows
+dd=map(insFiles,read_insdat) %>% bind_rows %>% group_by(Sample) %>% mutate(Density=Count/sum(Count))
+
 pg=ggplot(dd,aes(insert_size,Count,color=Sample)) +
     theme_light(base_size=16) +
     geom_line() +
     scale_x_continuous(breaks=c(0,rep(1:10)*100)) +
     scale_color_brewer(palette="Paired")
 
+pg2=ggplot(dd,aes(insert_size,Count,color=Sample)) +
+    theme_light(base_size=16) +
+    geom_line() +
+    scale_color_brewer(palette="Paired") +
+    scale_x_continuous(breaks=c(0,rep(1:10)*100))
+
+
 pdf(file="postInsDistribution.pdf",width=11,height=8.5)
 print(pg)
+print(pg2)
 dev.off()
