b70b9a7b0ded8647917803d5a5baba038fcd707c
Nicholas D. Socci <soccin@mskcc.org>
2022-06-16 19:04:24 -0400

    Update ins plot

--- plotINSStats.R
+++ plotINSStats.R
@@ -3,29 +3,36 @@ read_insdat<-function(ff) {
     start=grep("^insert_size",rr)
     read_tsv(paste0(rr[start:len(rr)],collapse="\n")) %>%
         gather(Sample,Count,-insert_size) %>%
-        mutate(Sample=gsub("\\.fr_count","",Sample))
+        mutate(Sample=gsub("\\.fr_count","",Sample)) %>%
+        rename(MapID=Sample)
 }
 
 suppressPackageStartupMessages(require(tidyverse))
 
 
-insFiles=dir(pattern="___INS.txt")
-dd=map(insFiles,read_insdat) %>% bind_rows %>% group_by(Sample) %>% mutate(Density=Count/sum(Count))
+insFiles=dir("out/metrics",pattern="___INS.txt",full.names=T)
+dd=map(insFiles,read_insdat) %>% bind_rows %>% group_by(MapID) %>% mutate(Density=Count/sum(Count))
 
-pg=ggplot(dd,aes(insert_size,Count,color=Sample)) +
-    theme_light(base_size=16) +
-    geom_line() +
-    scale_x_continuous(breaks=c(0,rep(1:10)*100)) +
-    scale_color_brewer(palette="Paired")
+manifest=read_csv("sampleManifest.csv") %>% group_by(Group) %>% mutate(Rep=as.factor(row_number())) %>% ungroup
+dd=left_join(dd,manifest)
 
-pg2=ggplot(dd,aes(insert_size,Density,color=Sample)) +
+pg1=ggplot(dd,aes(insert_size,1000*Density,color=Group,group=SampleID)) +
     theme_light(base_size=16) +
-    geom_line() +
-    scale_color_brewer(palette="Paired") +
+    geom_line(aes(linetype=Rep)) +
+    scale_color_brewer(palette="Set1") +
     scale_x_continuous(breaks=c(0,rep(1:10)*100))
+
+minDensity=10^ceiling(log10(max(dd$Density)/1e3))
+
+pg2=ggplot(dd,aes(insert_size,Density,color=Group,group=SampleID)) +
+    theme_light(base_size=16) +
+    geom_line(aes(linetype=Rep)) +
+    scale_color_brewer(palette="Set1") +
+    scale_x_continuous(breaks=c(0,rep(1:10)*100)) +
+    scale_y_log10(limits=c(minDensity,NA))
 
 
 pdf(file="postInsDistribution.pdf",width=11,height=8.5)
-print(pg)
+print(pg1)
 print(pg2)
 dev.off()
