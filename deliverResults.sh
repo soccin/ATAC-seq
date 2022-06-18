4a1d6e1854b4d22d70e01594ca75fbbc3a1b0cd3
Nicholas D. Socci <soccin@mskcc.org>
2022-06-18 17:27:26 -0400

    Updates to delivery email

--- deliverResults.sh
+++ deliverResults.sh
@@ -12,25 +12,15 @@ if [ "$#" != "1" ]; then
     exit
 fi
 
-echo $RESDIR
+echo \$RESDIR=$RESDIR $(realpath $RESDIR)
 
 echo
 echo "sudo needed to set permissions"
 echo
 sudo chmod g+ws $RESDIR
 
-mkdir -p $RESDIR/atacSeq/atlas
-mkdir -p $RESDIR/atacSeq/bigwig
-mkdir -p $RESDIR/atacSeq/macs
+rsync -avP atacSeq $RESDIR
 
-rsync -rvP *.bw $RESDIR/atacSeq/bigwig
-cp macsPeaksMerged.saf $RESDIR/atacSeq/atlas
-cat macsPeaksMerged.saf | awk '{print $2,$3,$4,$1,".",$5}' | tr ' ' '\t' >$RESDIR/atacSeq/atlas/macsPeaksMerged.bed
-rsync -rvP callpeaks/* $RESDIR/atacSeq/macs
-
-
-#mkdir -p $RESDIR/atacSeq/diffpeaks
-
-ATAC_PROJECT_NUM=$($SDIR/extractProjectIDFromPath.py $RESDIR | sed 's/^Proj_//')
+ATAC_PROJECT_NUM=$($SDIR/extractProjectIDFromPath.py $(realpath $RESDIR) | sed 's/^Proj_//')
 
 cat $SDIR/tmpldeliveryEmail.txt | sed 's@ATAC_PROJECT_NUM@'"$ATAC_PROJECT_NUM"'@'
